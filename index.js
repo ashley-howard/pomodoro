543b89e659f22dd17b08b6c2b4edf91721db0379
noteconvert <me@ashley.how>
2020-02-01 13:02:22 +0000

    fix pause

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 // Bugs:
 // - Pause doesn't work
-// - stuck on long breaks
-
-// set seconds to shortbreak
-// set seconds back to pomodoro
-// after 4 times, do long break
 
 const pomodoroTime = document.getElementById("pomodoro-input");
 const shortBreakTime = document.getElementById("short-break-input");
@@ -17,14 +12,36 @@ var counter = 0;
 var minutes = 0;
 var seconds = 0;
 var interval;
+var paused;
 
 function pomodoro() {
-    if (counter < 3) {
-        counter += 1;
-        console.log(counter)
-        console.log("pomodoro")
+    // if (paused === true) {
+    //     console.log("timer unpaused")
+    //     paused = false;
+    //     interval = setInterval(function () {
+    //         seconds--;
+    //         countdown.innerText = `${(minutes = Math.floor(seconds / 60))}:${seconds -
+    //             minutes * 60}`;
+    //         if (!seconds) {
+    //             clearInterval(interval);
+    //             shortBreak()
+    //         }
+    //     }, 1000);
+    // }
 
-        seconds = pomodoroTime.value * 60 || 0;
+    if (counter < 3) {
+
+        if (paused === true) {
+            console.log("timer unpaused")
+            paused = false;
+        }
+        else {
+            counter += 1;
+            console.log(counter)
+            console.log("pomodoro")
+            seconds = pomodoroTime.value * 60 || 0;
+        }
+
         interval = setInterval(function () {
             seconds--;
             countdown.innerText = `${(minutes = Math.floor(seconds / 60))}:${seconds -
@@ -84,12 +101,20 @@ function longBreak() {
 }
 
 function pauseTimer() {
-    clearInterval(interval);
+    if (!paused) {
+        console.log('timer paused')
+        paused = true;
+        clearInterval(interval);
+    } else {
+        console.log('already paused!')
+    }
 }
 
 function resetTimer() {
+    console.log('timer reset')
     clearInterval(interval);
-    updateTimer();
+    updateSettings();
+    counter = 0;
 }
 
 function updateSettings() {
@@ -98,10 +123,12 @@ function updateSettings() {
     } else {
         countdown.innerText = `${pomodoroTime.value}:00`;
     }
+    counter = 0;
 }
 
 function resetSettings() {
     pomodoroTime.value = 15;
     shortBreakTime.value = 5;
     longBreakTime.value = 10;
+    counter = 0;
 }
